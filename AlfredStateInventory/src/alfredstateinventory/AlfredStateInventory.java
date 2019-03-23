f3ecd80d6106b766ccb47236fef83ff7ec0ac935
Brandon M Hurst <hurstbm@alfredstate.edu>
2019-03-23 16:19:46 -0400

    Initial commit

---
+++ AlfredStateInventory/src/alfredstateinventory/AlfredStateInventory.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alfredstateinventory;
+
+import alfredstateinventory.userinterface.MainWindow;
+import alfredstateinventory.userinterface.PanelDetails;
+import alfredstateinventory.userinterface.PanelSignIn;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author BHAsus
+ */
+public class AlfredStateInventory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow mainW = new MainWindow();
+        mainW.main(args);
+        PanelDetails detail = new PanelDetails();
+        mainW.addPanel(detail);
+        
+    }
+    
+}
